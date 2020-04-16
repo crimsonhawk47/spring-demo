6c3771e194f3f67a0cb035b297caba974a789606
Gabriel Hawk <crimsonhawk47@gmail.com>
2020-04-16 15:52:31 -0500

    Setup basic classes, working through bean XML config

---
+++ src/com/luv2code/springdemo/HelloSpringApp.java
@@ -0,0 +1,13 @@
+package com.luv2code.springdemo;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+		
+		//Load spring configuration file
+		
+		//Retrieve bean from spring container
+
+	}
+
+}
